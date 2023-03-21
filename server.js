92e9d0f9ce8d8af7b0c3e27e5ce3b948cfaea18e
Wesner M <wesnerm@gmail.com>
2023-03-21 19:06:38 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const app = express();
 const port = 4000;
 
 app.get("/greeting/:name", (req, res) => {
-    const name = req.params.name;
+    const name = req.params.name;  // this took me some time to remember to do this , I kept onb using just req.params at first
   res.send("Hello there, " + name);
-
-
 });
 
-app.listen(port, () => {
+app.get("/tip/:total/:tipPercentage", (req,res) =>{
+    res.send( req.params.tipPercentage )
+    console.log(tip,"tip" ) 
+})
+ 
+
+app.listen(port, () => { 
   console.log(`Server is listening to PORT ${port} `);
 });
